acc4bcb3db5487721604f95c8d4cf38d9cdd6f25
Ian Foster <idfoster@sysnet66.ucsd.edu>
2013-01-29 01:26:21 -0800

    added code to track domains that fail

--- SpiderWho.py
+++ SpiderWho.py
@@ -6,12 +6,15 @@ import threading
 import time
 
 class WhoisThread(threading.Thread):
-  def __init__(self,proxy,port,queue):
+  def __init__(self,proxy,port,queue,fail):
     threading.Thread.__init__(self)
     self.client = NICClient()
     #TODO add better roxy type handling
     self.client.set_proxy(self.client.PROXY_TYPE_HTTP,proxy,port)
     self.queue = queue
+    self.wait = 25 #TODO change this
+    self.folder = "whois/"
+    self.fail = fail
 
   def whois(self,domain):
     #always use the native python client
@@ -21,8 +24,10 @@ class WhoisThread(threading.Thread):
   #TODO write this
   def save_data(self,domain,text):
     #save to a file
-    print text
-    return
+    f = open(self.folder+domain,'w')
+    f.write(text)
+    f.close()
+    #print text
 
   def run(self):
     while True:
@@ -30,15 +35,21 @@ class WhoisThread(threading.Thread):
       domain = self.queue.get()
 
       #TOOD rm
-      print "WHOIS: " + domain
-    
-      data = self.whois(domain)
-      self.save_data(domain,data)
+      #print "WHOIS: " + domain
+      try:
+        data = self.whois(domain)
+      except:
+        print "FAILED: [" + domain + "]"
+        self.fail.append(domain)
+      else:
+        print "SUCSESS: [" + domain + "]"
+        self.save_data(domain,data)
 
       #inform the queue we are done
       self.queue.task_done()
-
-      time.sleep(20) #update this
+      
+      if not q.empty():
+        time.sleep(self.wait)
 
 
 if __name__ == '__main__':
@@ -46,7 +57,9 @@ if __name__ == '__main__':
   if not len(sys.argv) == 3:
     print "usage: " + sys.argv[0] + " proxy_list domain_list"
     exit()
-  
+ 
+  fail_list = list()
+
   q = Queue.Queue()
   i = 0
   print "Starting threads.."
@@ -55,7 +68,7 @@ if __name__ == '__main__':
         s = l.split()
         if len(s) == 2:
           #TODO validate!
-          t = WhoisThread(s[0],int(s[1]),q)
+          t = WhoisThread(s[0],int(s[1]),q,fail_list)
           t.setDaemon(True)
           t.start()
           i += 1
@@ -69,8 +82,17 @@ if __name__ == '__main__':
   print "done " + str(i) +" domains in queue"
 
   print "All threads running"
-  while threading.active_count() > 0:
-      time.sleep(0.1)
-  #q.join()
+  '''while threading.active_count() > 0:
+      print "T: " + str(threading.active_count())
+      time.sleep(0.1)'''
+  q.join() # find a way to kill this
   print "Done!"
+
+  print "saving " +str(len(fail_list))+" fails"
+  fail_file = open("fail.txt",'a+')
+  for d in fail_list:
+    fail_file.write(d+'\n')
+  fail_file.close()
+  print "all done"
+
 
