f35afe5bc9b1099ef1a0f2a6fb0148bb9d505ef6
Ian Foster <idfoster@ucsd.edu>
2013-05-04 17:20:07 -0700

    inital use of record and proxy objects

--- helperThreads.py
+++ helperThreads.py
@@ -10,7 +10,7 @@ class ManagerThread(threading.Thread):
   def getWorkerThreadCount(self):
     return whoisThread.getWorkerThreadCount();
 
-   def __init__(self,proxy_list,domain_list):
+  def __init__(self,proxy_list,domain_list):
     threading.Thread.__init__(self)
     self.proxy_list = proxy_list
     self.domain_list = domain_list
@@ -27,7 +27,6 @@ class ManagerThread(threading.Thread):
   def run(self):
     #start FailThread
     self.fail_thread = FailThread(self.fail_file,self.fail_queue)
-    self.fail_thread.daemon = True
     self.fail_thread.start()
 
     #start whois threads
@@ -43,7 +42,9 @@ class ManagerThread(threading.Thread):
             except ValueError:
               print "Proxy "+ s[0] + " has non-int port"
             else:
-              t = whoisThread.WhoisThread(s[0],i,self.input_queue,self.fail_queue)
+              #TODO add better proxy type handling
+              proxy = whoisThread.Proxy(s[0],i,whoisThread.socks.PROXY_TYPE_HTTP)
+              t = whoisThread.WhoisThread(proxy,self.input_queue,self.fail_queue)
               t.start()
     except IOError:
       print "Unable to open proxy file: " + self.proxy_list
@@ -71,12 +72,14 @@ class ManagerThread(threading.Thread):
     #can I join a daemon thread?
     self.fail_queue.join()
 
+#TODO make a SaveThread
 
 #runs in the background and when an input fails it logs the bad 
 #data in a file
 class FailThread(threading.Thread):
   def __init__(self,filename,queue):
     threading.Thread.__init__(self)
+    self.daemon = True
     self._filename = filename
     self._queue = queue
     self._num_fails = 0
@@ -90,7 +93,7 @@ class FailThread(threading.Thread):
       self._num_fails += 1
       try:
         fail_file = open(self._filename,'a+')
-        fail_file.write(l+'\n')
+        fail_file.write(l.domain+'\n')
         fail_file.close()
       except IOError:
         print "Unabe to write to fail file"
@@ -122,6 +125,6 @@ class EnqueueThread(threading.Thread):
     for l in fh:
       l = l.strip()
       if len(l) > 3:
-        self._queue.put(l)
+        self._queue.put(whoisThread.WhoisResult(l))
         self._domains +=1
 
--- whoisThread.py
+++ whoisThread.py
@@ -3,7 +3,7 @@ import proxywhois
 import socks
 import sys #for debugging
 import time
-#from helperThreads import ManagerThread #circular import error
+import os
 
 debug = True
 
@@ -12,6 +12,11 @@ numWorkerThreads_lock = threading.Lock()
 numWorkerThreads = 0
 proxy_ip_list_lock = threading.Lock()
 proxy_ip_list = list()
+output_folder = "whois/"
+
+if not os.path.exists(output_folder):
+  os.makedirs(output_folder)
+
 
 def addRemoteProxyIP(ip):
   global proxy_ip_list_lock
@@ -28,6 +33,7 @@ def addRemoteProxyIP(ip):
     proxy_ip_list_lock.release()
     return ret
 
+
 def incrementWorkerThreadCount():
   global numWorkerThreads_lock
   global numWorkerThreads
@@ -37,6 +43,7 @@ def incrementWorkerThreadCount():
   finally:
     numWorkerThreads_lock.release()
 
+
 def decrementWorkerThreadCount():
   global numWorkerThreads_lock
   global numWorkerThreads
@@ -45,6 +52,7 @@ def decrementWorkerThreadCount():
     numWorkerThreads -= 1
   finally:
     numWorkerThreads_lock.release()
+
 
 def getWorkerThreadCount():
   global numWorkerThreads_lock
@@ -58,23 +66,53 @@ def getWorkerThreadCount():
   return ret
 
 
-class WhoisThread(threading.Thread):
-  def __init__(self,proxy,port,queue,fail):
-    threading.Thread.__init__(self)
-    self.daemon = True
-    self.client = proxywhois.NICClient()
-    #TODO add better proxy type handling
-    self.proxy_type = socks.PROXY_TYPE_HTTP
-    self.queue = queue
-    self.wait = 20 #TODO change this
-    self.folder = "whois/"
-    self.fail = fail
-    self.proxy_server = proxy
-    self.proxy_port = port
-    self.client.set_proxy(self.proxy_type,proxy,port)
-    self.running = True
 
-  def getExternalIP(self):
+#this object is used to store the results of a whois result as it is passed around
+class WhoisResult:
+  def __init__(self,domain):
+    self.domain = domain.upper()
+    self.proxies = list()
+    self.data = None
+  def setData(self,data):
+    self.data = data
+  def getData(self):
+    return self.data
+  def save(self):
+    f = open(output_folder+self.domain,'w')
+    f.write(self.data)
+    f.close()
+
+
+
+#class to hold a proxy object
+class Proxy:
+  def __init__(self,ip,port,proxy_type):
+    self.server = ip
+    self.port = port
+    self.proxy_type = proxy_type
+    self.external_ip = None
+    self.ready = False
+    self.errors = 0
+    self.client = proxywhois.NICClient()
+
+
+  def connect(self):
+    self.updateExternalIP()
+    self.client.set_proxy(self.proxy_type,self.server,self.port)
+    if not self.external_ip:
+      return False
+    self.ready = True
+    return self.ready
+
+
+  def __repr__(self):
+    ret = self.server +":"+str(self.port)
+    if self.external_ip:
+      ret += " ExternalIP: "+self.external_ip
+    return ret
+
+
+  def updateExternalIP(self):
     """this method uses the proxy socket to get the remote IP on that proxy"""
     host = "curlmyip.com"
     #host = "ipaddr.me"
@@ -83,73 +121,83 @@ class WhoisThread(threading.Thread):
     #host = "myip.dnsdynamic.com"
     try:
       s = socks.socksocket(socks.socket.AF_INET, socks.socket.SOCK_STREAM)
-      s.setproxy(self.proxy_type,self.proxy_server,self.proxy_port)
+      s.setproxy(self.proxy_type,self.server,self.port)
       s.connect((host,80))
       s.send('GET /\r\n\r\n')
       r = s.recv(4096)
     except socks.GeneralProxyError as e:
       return None
     else:
-      return r.split()[-1]
+      self.external_ip = r.split()[-1]
+      return self.external_ip
 
-
-  def whois(self,domain):
+  def whois(self,record):
+    if not self.ready:
+      return False
     #always use the native python client
-    text = self.client.whois_lookup(None, domain, 0)
-    return text
+    #TODO expand to sava data in result
+    #TODO log proxy query time, add proxy to record
+    text = self.client.whois_lookup(None, record.domain, 0)
+    record.setData(text)
 
-  def save_data(self,domain,text):
-    #save to a file
-    #TODO make this its own thread of (domain,text)
-    f = open(self.folder+domain,'w')
-    f.write(text)
-    f.close()
+#main thread which handles all whois lookups, one per proxy
+class WhoisThread(threading.Thread):
+  def __init__(self,proxy,queue,fail):
+    threading.Thread.__init__(self)
+    self.daemon = True
+    self.queue = queue
+    self.proxy = proxy
+    self.wait = 20 #TODO change this
+    self.fail = fail
+    self.running = True
+
 
   def run(self):
     incrementWorkerThreadCount()
     #get and print my remote IP, also tests the proxy for usability
-    ip = self.getExternalIP()
-    if not ip:
-      print "WARNING: Failed to connect to proxy: "+ self.proxy_server
+    if not self.proxy.connect():
+      print "WARNING: Failed to connect to proxy: "+ str(self.proxy)
       decrementWorkerThreadCount()
       return
     else:
       if debug:
-        print "Thread running with proxy: "+ self.proxy_server +" with remote IP: " + str(ip)
+        print "Thread running with proxy: "+ str(self.proxy)
 
-    if not addRemoteProxyIP(ip):
-      print "WARNING: Proxy: "+self.proxy_server+" with remote ip: "+ip+" is already being used"
+    if not addRemoteProxyIP(self.proxy.external_ip):
+      print "WARNING: Proxy: "+str(self.proxy)+" is already being used"
       decrementWorkerThreadCount()
       return
     
     while self.running:
       #get next host
-      domain = self.queue.get().upper()
-      if debug:
-        print "["+ip+"]got domain: " + domain
-
+      record = self.queue.get()
       try:
-        data = self.whois(domain)
+        self.proxy.whois(record)
       except proxywhois.socks.GeneralProxyError as e:
         if e.value[0] == 6: #is there a proxy error?
-          print "Unable to connect to proxy: "+ self.proxy_server +":"+ str(self.proxy_port)
+          print "Unable to connect to proxy: "+ str(self.proxy)
           self.running = False
-          self.queue.put(domain)
+          #TODO why?
+          self.queue.put(record)
         else:
-          print "Error Running whois on domain:["+domain+"] " + str(e)
-          self.fail.put(domain)
+          print "Error Running whois on domain:["+record.domain+"] " + str(e)
+          #TODO why?
+          self.fail.put(record)
       except proxywhois.socks.HTTPError as e:
         #TODO also handle the socks case
         #bad domain name
-        print "Invalid domain: " + domain
-        self.fail.put(domain)
+        print "Invalid domain: " + record.domain
+        #TODO why?
+        self.fail.put(record)
       except Exception as e:
-        print "FAILED: [" + domain + "] error: " + str(sys.exc_info()[0])
-        self.fail.put(domain)
+        print "FAILED: [" + record.domain + "] error: " + str(sys.exc_info()[0])
+        #TODO why?
+        self.fail.put(record)
       else:
         if debug:
-          print "SUCSESS: [" + domain + "]"
-        self.save_data(domain,data)
+          print "SUCSESS: [" + record.domain + "]"
+        #TODO move this to a save thread
+        record.save()
       finally:
         #inform the queue we are done
         self.queue.task_done()
