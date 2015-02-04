a7df63eafa68b9ed29d0c6242be41d4264249e9d
Ian Foster <mrlanrat@gmail.com>
2015-02-03 17:24:32 -0800

    fix bug with non clearing terminal

--- SpiderWho.py
+++ SpiderWho.py
@@ -20,6 +20,40 @@ def set_proc_name(newname):
     except:
         pass
 
+
+def getTerminalSize():
+    """
+    stolen from http://stackoverflow.com/questions/566746/how-to-get-console-window-width-in-python
+    returns (width, height)
+    """
+    import os
+    env = os.environ
+    def ioctl_GWINSZ(fd):
+        try:
+            import fcntl, termios, struct, os
+            cr = struct.unpack('hh', fcntl.ioctl(fd, termios.TIOCGWINSZ,
+        '1234'))
+        except:
+            return
+        return cr
+    cr = ioctl_GWINSZ(0) or ioctl_GWINSZ(1) or ioctl_GWINSZ(2)
+    if not cr:
+        try:
+            fd = os.open(os.ctermid(), os.O_RDONLY)
+            cr = ioctl_GWINSZ(fd)
+            os.close(fd)
+        except:
+            pass
+    if not cr:
+        cr = (env.get('LINES', 25), env.get('COLUMNS', 80))
+
+        ### Use get(key[, default]) instead of a try/catch
+        #try:
+        #    cr = (env['LINES'], env['COLUMNS'])
+        #except:
+        #    cr = (25, 80)
+    return int(cr[1]), int(cr[0])
+
 def print_status_line():
     '''prints the statusline header'''
     sys.stdout.write("Domains\tLookups\tGood\tFail\tSaved\tActive/Proxies\tLPS\tQsize\tTime")
@@ -49,6 +83,11 @@ def print_status_data(manager):
         q_size = str(manager.input_queue.qsize())
 
     last_lookups = lookups
+
+    #fill terminal with whitespace
+    (width, height) = getTerminalSize()
+    sys.stdout.write('\r' + (' ' * width))
+
 
     sys.stdout.write("\r%d  \t%d  \t%d  \t%d  \t%d  \t%d / %d  \t%.1f  \t%s \t%s " %
             (domains, lookups, good_saved, fail_saved, total_saved, active_threads, total_threads, lps, q_size, running_time))
